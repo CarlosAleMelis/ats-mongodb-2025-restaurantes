0ccb0de9f23d4ca5749f16800ce08ee1b23c0c37
CarlosAleMelis <1605420@uab.cat>
2025-03-19 11:09:22 +0100

    Add aggregation

---
+++ scripts/obligatori/aggregateLookUpRestaurantsAndInspections.js
@@ -0,0 +1,19 @@
+
+use("ATS-PRAC");
+
+db.getCollection("inspections")
+  .updateMany({}, [
+    { $set: { restaurant_id: { $toObjectId: "$restaurant_id" } } }
+  ])
+
+db.getCollection('restaurants')
+  .aggregate([
+    {
+      $lookup: {
+        from: "inspections",
+        localField: "_id",
+        foreignField: "restaurant_id",
+        as: "test"
+      }
+    }
+  ])
